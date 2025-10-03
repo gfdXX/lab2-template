c62fae6806357e79a194dce679a9a0723385841c
Daniil <salgikda@gmail.com>
2025-10-03 14:36:21 +0300

    Clean up project: remove unused files
    - Remove old SQL scripts (10-, 20-, 21-, 22-, 23- files)
    - Remove postgres/scripts/ directory with old versions
    - Remove unused v1, v2, v4 directories (keep only v3)
    - Remove Python __pycache__ directories
    - Keep only essential files: init-databases.sh, v3/, services/, scripts/
    - All Newman tests still pass after cleanup

--- postgres/20-create-databases.sql
+++
@@ -1,9 +0,0 @@
--- Create databases
-CREATE DATABASE cars;
-CREATE DATABASE rentals;
-CREATE DATABASE payments;
-
--- Grant privileges
-GRANT ALL PRIVILEGES ON DATABASE cars TO program;
-GRANT ALL PRIVILEGES ON DATABASE rentals TO program;
-GRANT ALL PRIVILEGES ON DATABASE payments TO program;
